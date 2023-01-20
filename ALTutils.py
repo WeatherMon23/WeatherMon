71e709816de8626b6e164bcd3fdc27001fb0226a
WeatherMon23 <116896890+WeatherMon23@users.noreply.github.com>
2023-01-20 19:14:49 +0200

    Added ALTutils
    ALTutils will contain all general helping functions.

---
+++ ALTutils.py
@@ -0,0 +1,40 @@
+
+
+################# UTILS ######################
+
+def capitalize_first_letter(s):
+    """
+    Capitilizes the first letter of each word in a given sentence.
+
+    Parameters
+    ----------
+    s : str
+        The sentence we want to capitalize.
+
+    Returns
+    -------
+    s : str
+        The modified sentence.
+    """
+    lst = [word[0].upper() + word[1:] for word in s.split()]
+    s = " ".join(lst)
+    return s
+
+def add_left_zero(s):
+    """
+    Adds '0' to the left of a string.
+
+    Parameters
+    ----------
+    s : str
+        The string we want to modify.
+
+    Returns
+    -------
+    : str
+        The string with a '0' concatentated to its left.
+    """
+    if len(s) == 1:
+        return '0' + s
+    else:
+        return s
