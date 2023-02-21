258c67507989d2cf2e6d4b4f42e2bbd30550449f
WeatherMon23 <116896890+WeatherMon23@users.noreply.github.com>
2023-02-21 17:21:22 +0200

    Update ALTweb_server.py

--- ALTweb_server.py
+++ ALTweb_server.py
@@ -1,167 +1,50 @@
-from m5stack import *
-from uiflow import *
-from m5ui import *
-import wifiCfg
 import socket
-
+from urllib.parse import urlparse, parse_qs
+import wifiCfg
+from ALTconnection import *
 from ALTutils import urlDecode, extract_string
 from ALTwidgets import Alert
 
 
-def get_user_input(SSID, password):
-    ip = wifiCfg.wlan_sta.ifconfig()
+def get_user_input(port=80, alert_msg='Navigate to:'):
+    check_connection()
 
-    wifiCfg.doConnect(SSID, password)
+    ip = wifiCfg.wlan_sta.ifconfig()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((ip[0], 80))
+    s.bind((ip[0], port))
     s.listen(5)
 
-    msg = ''
-    if not wifiCfg.wlan_sta.isconnected():
-        return ''
+    Alert(alert_msg + ' http://' + str(ip[0]) + ':' + str(port) + '/submit', 0x000000, '', 0x000000, 0xFFFFFF, None,
+          None)
 
-    Alert('Type https://' + str(ip[0]) + '/ in Your Browser', 0x000000, '', 0x000000, 0xFFFFFF, None, None)
+    while 1:
+        input_var = None
+        conn, addr = s.accept()
+        request = conn.recv(1024)
+        request = urlDecode(str(request))
+        parsed_url = urlparse(extract_string(request, 'GET '))
+        requested_path = parsed_url.path
+        if requested_path == '/submit':
+            input_var = parse_qs(parsed_url.query).get('input', [None])[0]
+            if input_var:
+                html_file = open("./Assets/HTML/submitted.html", "r")
+                response = html_file.read()
+            else:
+                html_file = open("./Assets/HTML/submit.html", "r")
+                response = html_file.read()
+        elif requested_path == '/favicon.ico':
+            html_file = open("./Assets/HTML/favicon.ico", "rb")
+            response = html_file.read()
+        else:
+            html_file = open("./Assets/HTML/not_found.html", "r")
+            response = html_file.read()
 
-    conn, addr = s.accept()
-    request = conn.recv(1024)
-    request = urlDecode(str(request))
-    user_input = extract_string(request, '?input=')
+        conn.send('HTTP/1.1 200 OK\n')
+        conn.send('Content-Type: text/html\n')
+        conn.send('Connection: close\n\n')
+        conn.sendall(response)
+        conn.close()
 
-    response = '''
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>User Input Page</title>
-    <style>
-      /* CSS styles */
-      body {
-        font-family: Arial, sans-serif;
-        background-color: #f8f8f8;
-        margin: 0;
-      }
-
-      .container {
-        max-width: 600px;
-        margin: 0 auto;
-        padding: 40px;
-        background-color: #fff;
-        border-radius: 8px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        text-align: center;
-      }
-
-      h1 {
-        font-size: 32px;
-        margin-bottom: 20px;
-      }
-
-      input[type="text"] {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        box-sizing: border-box;
-        border: 2px solid #ccc;
-        border-radius: 4px;
-        font-size: 16px;
-        transition: border-color 0.3s;
-      }
-
-      input[type="text"]:focus {
-        outline: none;
-        border-color: dodgerblue;
-      }
-
-      .btn {
-        background-color: dodgerblue;
-        color: #fff;
-        border: none;
-        padding: 12px 24px;
-        border-radius: 4px;
-        font-size: 16px;
-        cursor: pointer;
-        transition: background-color 0.3s;
-      }
-
-      .btn:hover {
-        background-color: #0066cc;
-      }
-
-      .error {
-        border-color: red;
-      }
-
-      .datetime {
-        font-size: 20px;
-        margin-top: 40px;
-        color: #555;
-      }
-
-      .time {
-        font-weight: bold;
-        color: dodgerblue;
-        font-size: 36px;
-        margin-bottom: 10px;
-      }
-
-      .date {
-        text-transform: uppercase;
-        letter-spacing: 2px;
-        font-size: 18px;
-        color: #999;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="container">
-      <h1>Enter your input:</h1>
-      <form id="myForm">
-        <input type="text" id="userInput" placeholder="Your input here...">
-        <button class="btn" type="submit">Submit</button>
-      </form>
-      <div class="datetime">
-      <div class="time" id="currentTime"></div>
-      <div class="date" id="currentDate"></div>
-    </div>
-    </div>
-    <script>
-      function submitInput(event) {
-        event.preventDefault();
-        const userInput = document.getElementById("userInput");
-        const u_input = userInput.value;
-        if (u_input === "") {
-          userInput.classList.add("error");
-          userInput.setAttribute("placeholder", "Please enter your input");
-        } else {
-          window.location.href = window.location.href + "?input=" + encodeURIComponent(u_input);
-        }
-      }
-
-      function updateTime() {
-        const now = new Date();
-        const time = now.toLocaleTimeString([], {
-          hour: "numeric",
-          minute: "2-digit",
-          hour12: true
-        });
-        const date = now.toLocaleDateString([], {
-          weekday: "long",
-          year: "numeric",
-          month: "long",
-          day: "numeric"
-        });
-        document.getElementById("currentTime").innerHTML = time;
-        document.getElementById("currentDate").innerHTML = date;
-      }
-      setInterval(updateTime, 1000); // call updateTime every second
-      document.getElementById("myForm").addEventListener("submit", submitInput);
-    </script>
-  </body>
-    '''
-    conn.send('HTTP/1.1 200 OK\n')
-    conn.send('Content-Type: text/html\n')
-    conn.send('Connection: close\n\n')
-    conn.sendall(response)
-    conn.close()
-
-    return user_input
+        if input_var:
+            s.close()
+            return input_var
