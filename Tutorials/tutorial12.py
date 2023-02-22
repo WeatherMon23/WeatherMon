21d859367f5c7983c584274d93e874634c1c74d4
WeatherMon23 <116896890+WeatherMon23@users.noreply.github.com>
2023-02-22 19:22:27 +0200

    Add files via upload

---
+++ Tutorials/tutorial12.py
@@ -0,0 +1,28 @@
+'''
+Tutorial 12 example -- publisher (MQTT)
+'''
+from m5stack import *
+from m5mqtt import M5mqtt
+from ALTelements import *
+from ALTconnection import *
+import time
+
+
+## Creating the screen we want to display our content at ##
+screen = M5Screen()
+screen.clean_screen()
+screen.set_screen_bg_color(0xB6D0E2)
+
+wifi_SSD = "Cellcom-WiFi_01"
+wifi_pass = "0503322407"
+
+## Connecting to wifi ##
+connect_wifi(wifi_SSD, wifi_pass)
+m5mqtt = M5mqtt('kjgwdd', 'io.adafruit.com', 1883, 'WeatherMon', 'aio_Hzae8396FuVde9G4ORx1ap1nWmdy', 300)
+m5mqtt.start()
+
+var = 0
+for count in range(10):
+  var = var + 1
+  m5mqtt.publish(str('WeatherMon/feeds/exampleFeed'), str(var), 1)
+  time.sleep(1)
